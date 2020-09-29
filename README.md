9d4bac48852b32daea3e932d42a9a1f791722fa2
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2020-09-29 01:24:52 -0400

    realize you def need to know way more math for this

---
+++ README.md
@@ -0,0 +1,10 @@
+# I Just Want To Understand A Tesseract
+
+Here is the basic video I watched
+https://pythonprogramming.net/opengl-pyopengl-python-pygame-tutorial/
+
+In order to get this to actually run do:
+
+```bash
+python3 main.py
+```
--- main.py
+++ main.py
@@ -30,6 +30,43 @@ edges = (
     (5,7)
     )
 
+#     y
+#     |
+#     |
+#     |
+#     |
+#       ------------ X
+#    /
+#   /
+#  /
+# z
+
+    # (x, y, z)
+verticies2 = [
+    [1, -1, -1], # back bottom right
+    [1, 1, -1],  # back top right
+    [-1, 1, -1], # back top left
+    [-1, -1, -1], # back bottom left
+    [1, -1, 1],   # front bottom right
+    [1, 1, 1],    # front top right
+    [-1, -1, 1],  # front bottom left
+    [-1, 1, 1]    # front top left
+]
+
+def Cube2(transform):
+    verticies2[0][0] = transform * .1 + 1
+    verticies2[0][2] = transform * .1 + 1
+    verticies2[1][0] = transform * .1 + 1
+    verticies2[2][0] = transform * -.1 - 1
+    verticies2[3][0] = transform * -.1 - 1
+    glBegin(GL_LINES)
+    for idx, edge in enumerate(edges):
+        for vertex in edge:
+            new_verts = []
+            for vert in verticies2[vertex]:
+                new_verts.append(vert)
+            glVertex3fv(new_verts)
+    glEnd()
 
 def Cube():
     glBegin(GL_LINES)
@@ -38,27 +75,32 @@ def Cube():
             glVertex3fv(verticies[vertex])
     glEnd()
 
-
 def main():
     pygame.init()
     display = (800,600)
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
 
     gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+    glTranslatef(0.0,0.0, -10)
 
-    glTranslatef(0.0,0.0, -5)
-
+    count = 1
     while True:
+        # Quit the "game" with an x
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
 
-        glRotatef(1, 3, 1, 1)
+        # glRotatef(0, 0, 0, 0) # no movement
+        # glRotatef(1, 0, 0, 0)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-        Cube()
+        # Cube()
+        Cube2(count)
+        # redraws the screen
         pygame.display.flip()
-        pygame.time.wait(10)
-
+        pygame.time.wait(100)
+        if count > 20:
+            count = 1
+        count += 1
 
 main()
